6939b335f47422d2453de5d6b94d7f2ff07319d7
Eugen Karaman <karamanjekakol@gmail.com>
2024-06-11 19:16:02 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Weather_task
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new WeatherViewModel();
+        }
+    }
+    public class WeatherInfo
+    {
+        public string City { get; set; }
+        public double Temperature { get; set; }
+        public string Description { get; set; }
+    }
+    public class WeatherService
+    {
+        public WeatherInfo GetWeather(string city)
+        {
+            // Для демонстрации используем фиктивные данные
+            var random = new Random();
+            return new WeatherInfo
+            {
+                City = city,
+                Temperature = random.Next(-10, 30),
+                Description = "Ясно"
+            };
+        }
+    }
+
+}
---
+++ WeatherViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Weather_task;
+
+public class WeatherViewModel : INotifyPropertyChanged
+{
+    private readonly WeatherService weatherService;
+    private string selectedCity;
+    private WeatherInfo weatherInfo;
+
+    public WeatherViewModel()
+    {
+        weatherService = new WeatherService();
+        Cities = new List<string> { "Moscow", "London", "New York", "Tokyo", "Paris" };
+        SelectedCity = Cities[0];
+        UpdateWeather();
+    }
+
+    public List<string> Cities { get; }
+
+    public string SelectedCity
+    {
+        get => selectedCity;
+        set
+        {
+            selectedCity = value;
+            OnPropertyChanged();
+            UpdateWeather();
+        }
+    }
+
+    public WeatherInfo WeatherInfo
+    {
+        get => weatherInfo;
+        private set
+        {
+            weatherInfo = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private void UpdateWeather()
+    {
+        WeatherInfo = weatherService.GetWeather(SelectedCity);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
